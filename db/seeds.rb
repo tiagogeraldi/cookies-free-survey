58fa7c759be722f2c679afb0a1d8e92b228e7076
Tiago Geraldi <tiagogeraldi@msn.com>
2023-03-12 18:08:31 -0300

    Adding seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating a survey..."
+
+quiz = Quizer::Quiz.new description: Faker::Quotes::Shakespeare.hamlet_quote
+quiz.generate_secrets
+quiz.save!
+
+Quizer::Question.question_types.keys.each.with_index do |question_type, index|
+  question = quiz.questions.new question_type: question_type, position: index + 1
+  question.description = Faker::Quotes::Shakespeare.king_richard_iii
+  question.save!
+
+  if !question.descriptive?
+    4.times do
+      question.alternatives.create!(
+        description: Faker::Quotes::Shakespeare.romeo_and_juliet,
+        correct: [true, false].sample
+      )
+    end
+  end
+end
+
+# 10 people answered the survey
+10.times do
+  quiz.questions.each do |question|
+    answer = Quizer::Answer.new(
+      quiz: quiz,
+      question: question,
+      session_hex: SecureRandom.hex(10)
+    )
+    if !question.descriptive?
+      answer.alternatives << question.alternatives.sample
+    else
+      answer.descriptive = Faker::Quotes::Shakespeare.as_you_like_it
+    end
+
+    answer.save!
+  end
+end
+
+include Rails.application.routes.url_helpers
+default_url_options[:host] = 'http://localhost:3000'
+
+puts "Owner link:"
+puts url_for([quiz, { only_path: false }])
+puts "Audience link:"
+puts quizer_answers_url(s: quiz.audience_secret, only_path: false)
