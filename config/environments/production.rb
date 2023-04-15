3b4d67344c938f341554bf49be3b937974db554f
Tiago Geraldi <tiagogeraldi@msn.com>
2023-04-15 18:01:09 -0300

    Change Paypal url production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paypal_url = 'https://api-m.sandbox.paypal.com'
+  config.paypal_url = 'https://api-m.paypal.com'
 end
