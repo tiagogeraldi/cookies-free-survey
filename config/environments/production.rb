84a12c8992f1cf38e9f665d0d4e847ceb75bb3f2
Tiago Geraldi <tiagogeraldi@msn.com>
2023-04-15 16:08:44 -0300

    Undo production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :terser
+  #config.assets.css_compressor = :yui
+  #config.assets.js_compressor = :terser
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
